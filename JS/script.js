580ee1e8a176d61aafbf2e89cee9337b9c88c987
Tiffany Mitchell <mitchelltiffany0@gmail.com>
2019-06-14 09:38:07 -0400

    attach jquery to html

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+// var gridWidth = 5;
+// var gridHeight = 6;
+// var grid = [];
+// for(var y = 0; y < gridHeight; y++)
+// {
+//     grid.push([]);
+//     for(var x = 0; x < gridWidth; x++)
+//     {
+//         grid[y].push(0);
+//     }
+// }
+
+
+$("#StartButton").click(function () {
+    $("#SplashScreen").hide();
+    $("#GameCanvas").show();
+    console.log("hello")
+});
+
+
